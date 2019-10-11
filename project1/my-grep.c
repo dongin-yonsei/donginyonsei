452efcfdccb7b9f2eeb4bfbba6fab78fede13300
dongin-yonsei <dongin.kim@yonsei.ac.kr>
2019-10-11 15:32:57 +0900

    my-grep modification

--- project1/my-grep.c
+++ project1/my-grep.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
   		}
 
   		// read each line until reaching the last line of the file
-  		while (read = getline(&line, &len, fp) != -1) {
+  		while ((read = getline(&line, &len, fp)) != -1) {
   			line_counter++;
   			if(find(argv[1],line)) {
   				printf("%d:%s",line_counter, line);
